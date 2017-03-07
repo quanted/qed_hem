000c8c92bf7be1c09b93fe18e243e989dd812b93
dmlyons2 <dmlyons2@gmail.com>
2017-03-07 14:27:41 -0500

    Admin interface added

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 
 #regular expressions
 # the r in r'^cts/index.html$' indicates that what is inside the quotes is a regular expression
 # the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
-# the $ in r'^cts/index.html$' indicates that we are looking to extend end the mathing part exactly here
+# the $ in r'^cts/index.html$' indicates that we are looking to extend end the matching part exactly here
 
 print('qed.urls')
 #appends to the list of url patterns to check against
@@ -13,4 +14,5 @@ urlpatterns = [
     #url(r'^hwbi/', include('hwbi_app.urls')),
     #url(r'^cts/', include('cts_app.urls')),
     #url(r'^ubertool/', include('ubertool_app.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
