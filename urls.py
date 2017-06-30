6008b13b3604dee0a9dfde82d753b7c9d37dc807
dmlyons2 <dmlyons2@gmail.com>
2017-06-30 12:40:19 -0400

    Removing auto ordering to speed up query time

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 #regular expressions
 # the r in r'^cts/index.html$' indicates that what is inside the quotes is a regular expression
 # the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
 # the $ in r'^cts/index.html$' indicates that we are looking to extend end the matching part exactly here
 
-print('qed.urls')
 #appends to the list of url patterns to check against
 urlpatterns = [
 	url(r'^', include('splash_app.urls')),
@@ -16,5 +16,11 @@ urlpatterns = [
     #url(r'^ubertool/', include('ubertool_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+	import debug_toolbar
+	urlpatterns = [
+		url(r'^__debug__/', include(debug_toolbar.urls)),
+	] + urlpatterns
 
 
